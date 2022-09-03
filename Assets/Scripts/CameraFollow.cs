9d8c80de61fa33489adba4d9554c752825a2e4cf
KimiyaPoozesh <86666504+KimiyaPoozesh@users.noreply.github.com>
2022-09-03 23:21:18 +0430

    SetZoom updated
    now we can change the camera field of view with right click

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -14,12 +14,37 @@ public class CameraFollow : MonoBehaviour
     [SerializeField]
     private Vector3 offset;
 
+    [SerializeField]
+    public float zoomMultiplier = 2;
+    public float defaultFov = 60;
+    public float zoomDuration = 2;
+    [SerializeField]
+    public float zoomSpeed = 2;
+    public Camera cam;
+
+    void Update(){
+        if (Input.GetMouseButton(1))
+        {
+            SetZoom(zoomSpeed,defaultFov / zoomMultiplier);
+        }
+        else if (cam.fieldOfView != defaultFov)
+        {
+            SetZoom(zoomSpeed,defaultFov);
+        }
+    }
+
     void LateUpdate(){
         Vector3 desiredPosition = target.position + offset;
-        SetZoom(followSmoothing,desiredPosition);
+        transform.position = Vector3.Lerp(transform.position,desiredPosition,followSmoothing);
     }
-    public void SetZoom(float zoomSpeed, Vector3 targetZoom){
-        transform.position = Vector3.Lerp(transform.position,targetZoom,zoomSpeed);
+
+    
+    public void SetZoom(float zoomSpeed, float targetZoom){
+        float angle = Mathf.Abs((defaultFov / zoomMultiplier) - defaultFov);
+        cam.fieldOfView = Mathf.MoveTowards(cam.fieldOfView, targetZoom, angle / zoomDuration * zoomSpeed);
+        
+        
     }
+        
 
 }
