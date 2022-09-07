8fa6daa54e7e202545e4e16be81e3192a481473a
KimiyaPoozesh <86666504+KimiyaPoozesh@users.noreply.github.com>
2022-09-07 11:51:53 +0430

    Enemy manager + gitignore
    first enemy manager attempt + small update to gitignore

---
+++ Assets/Scripts/EnemyManager.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+
+    // private PathFinding pathFinding;
+    private Shooting shooting;
+    private Damageable damageable;
+    private bool isInArea;
+
+    [SerializeField]
+    private float shootRange;
+    [SerializeField]
+    private float damageValue;
+    public SphereCollider collider;
+    
+    void Start()
+    {
+        collider.radius = shootRange;
+    }
+
+    void Update(){
+
+        if(isInArea){
+            Debug.Log("Found in Area!");
+            
+
+        } 
+        
+    }
+    void OnTriggerStay(Collider other){
+
+        if(other.CompareTag("Player")){
+            isInArea = true;
+            shooting.Shoot(other.transform);
+            // pathFinding.following=true;
+            // pathFinding.SetTarget(other.transform);
+        }
+    void OnCollisionEnter(Collision collision){
+        if (collision.gameObject.CompareTag("Bullet")){
+            damageable.GetDamaged(damageValue);
+            Destroy(collision.gameObject);
+        }
+    }
+        
+    }
+    void OnTriggerExit(Collider other){
+        if(other.CompareTag("Player")){
+            isInArea = false;
+            // pathFinding.following=false;
+        }       
+    }
+        
+
+}
