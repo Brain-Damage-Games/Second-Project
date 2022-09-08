1c43903d7bd5de592307b8000d6f364698c4f2b4
saragholamii <96429368+saragholamii@users.noreply.github.com>
2022-09-08 13:26:03 +0430

    shooting Version-1.1

--- Assets/Scripts/Shooting.cs
+++ Assets/Scripts/Shooting.cs
@@ -14,10 +14,53 @@ public class Shooting : MonoBehaviour
     [SerializeField]
     Transform gun;
 
-    public void Shoot(Transform shootTarget)
+    [SerializeField]
+    bool shoot;
+
+    [SerializeField]
+    float coolDown = 1f;
+
+    float pasedTime = 0f;
+
+    [SerializeField]
+    Transform shootTarget;
+
+    private void Update() 
     {
-        GameObject bullet = Instantiate(bulletPrefab, gun.position, Quaternion.identity);
-        bullet.GetComponent<Rigidbody>().velocity = (shootTarget.position - gun.position).normalized  * shootingSpeed;
+        if(Input.GetMouseButtonDown(0))
+        {
+            Shoot();
+        }
     }
+
+    public void Shoot()
+    {
+        print("inside shoot");
+        while(shoot)
+        {
+            print("inside while");
+            StartCoroutine(Wait());
+            GameObject bullet = Instantiate(bulletPrefab, gun.position, Quaternion.identity);
+            bullet.GetComponent<Rigidbody>().velocity = (shootTarget.position - gun.position).normalized  * shootingSpeed;
+        }
+    }
+
+    IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(coolDown);
+    }
+
+    public void SetShootTarget(Transform shootTarget)
+    {
+        this.shootTarget = shootTarget;
+    }
+
+    public void SetShoot(bool shoot)
+    {
+        this.shoot = shoot;
+    }
+
+
+
 
 }
